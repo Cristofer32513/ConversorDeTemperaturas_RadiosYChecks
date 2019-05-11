21a97d57ae4d5575dbd226f1ce66ae25d592fc1b
Cristofer32513 <cristofer32513@gmail.com>
2019-05-10 21:08:04 -0500

    Se creo el proyecto.

---
+++ app/src/main/java/itsj/programas/conversordetemperaturas_radiosychecks/MainActivity.java
@@ -0,0 +1,13 @@
+package itsj.programas.conversordetemperaturas_radiosychecks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
