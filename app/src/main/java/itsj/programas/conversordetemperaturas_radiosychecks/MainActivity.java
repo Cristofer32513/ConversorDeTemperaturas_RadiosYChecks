89294ba1224af737afe82a4c21fdaa17efb44f4d
Cristofer32513 <cristofer32513@gmail.com>
2019-05-10 21:18:14 -0500

    Se instanciaron las varibles del proyecto.

--- app/src/main/java/itsj/programas/conversordetemperaturas_radiosychecks/MainActivity.java
+++ app/src/main/java/itsj/programas/conversordetemperaturas_radiosychecks/MainActivity.java
@@ -2,6 +2,7 @@ package itsj.programas.conversordetemperaturas_radiosychecks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -21,5 +22,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cajaEntrada=findViewById(R.id.caja_entrada);
+
+        cajaC=findViewById(R.id.caja_c);
+        cajaF=findViewById(R.id.caja_f);
+        cajaK=findViewById(R.id.caja_k);
+        cajaR=findViewById(R.id.caja_r);
+
+        radioC=findViewById(R.id.radio_c);
+
+        radioF=findViewById(R.id.radio_f);
+
+        radioK=findViewById(R.id.radio_k);
+
+        radioR=findViewById(R.id.radio_r);
+
+        checkC=findViewById(R.id.check_c);
+
+        checkF=findViewById(R.id.check_f);
+
+        checkK=findViewById(R.id.check_k);
+
+        checkR=findViewById(R.id.check_r);
+
+        cajaC.setEnabled(false);
+        cajaF.setEnabled(false);
+        cajaK.setEnabled(false);
+        cajaR.setEnabled(false);
+
+
+
     }
 }
