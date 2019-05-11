a314e5a51b4ecb0eddae17b856bfab6a323967cb
Cristofer32513 <cristofer32513@gmail.com>
2019-05-10 21:29:33 -0500

    Se agrego el metodo convertir que se encarga de hacer todas las comparaciones posibles para realizar la convercion.

--- app/src/main/java/itsj/programas/conversordetemperaturas_radiosychecks/MainActivity.java
+++ app/src/main/java/itsj/programas/conversordetemperaturas_radiosychecks/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -62,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
                 habilitarCheckBoxes(true, true, true, false);
             }
         });
-        
+
         checkC=findViewById(R.id.check_c);
 
         checkF=findViewById(R.id.check_f);
@@ -78,6 +79,52 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
 
 
 
+    }
+    
+    public void convertir(){
+        limpiarCajas();
+        if(cajaEntrada.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Ingrese un valor para convertir.", Toast.LENGTH_SHORT).show();
+            limpiarCheckBoxes();
+        }
+        else if(radioC.isChecked() || radioF.isChecked() || radioK.isChecked() || radioR.isChecked()){
+            if(radioC.isChecked()) {
+                if(checkF.isChecked())
+                    cajaF.setText(redondear.format((tempEntrada*1.8)+32)+"");
+                if(checkK.isChecked())
+                    cajaK.setText(redondear.format(tempEntrada+273.15)+"");
+                if(checkR.isChecked())
+                    cajaR.setText(redondear.format((tempEntrada*1.8)+491.67)+"");
+            }
+            else if(radioF.isChecked()){
+                if(checkC.isChecked())
+                    cajaC.setText(redondear.format((tempEntrada-32)/1.8)+"");
+                if(checkK.isChecked())
+                    cajaK.setText(redondear.format((tempEntrada+459.67)/1.8)+"");
+                if(checkR.isChecked())
+                    cajaR.setText(redondear.format(tempEntrada+459.67)+"");
+            }
+            else if(radioK.isChecked()){
+                if(checkC.isChecked())
+                    cajaC.setText(redondear.format(tempEntrada-273.15)+"");
+                if(checkF.isChecked())
+                    cajaF.setText(redondear.format(((tempEntrada-273.15)*1.8)+32)+"");
+                if(checkR.isChecked())
+                    cajaR.setText(redondear.format(tempEntrada*1.8)+"");
+            }
+            else if(radioR.isChecked()){
+                if(checkC.isChecked())
+                    cajaC.setText(redondear.format((tempEntrada-491.67)/1.8)+"");
+                if(checkF.isChecked())
+                    cajaF.setText(redondear.format(tempEntrada-459.67)+"");
+                if(checkK.isChecked())
+                    cajaK.setText(redondear.format(tempEntrada/1.8)+"");
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Seleccione la unidad de medida de la temperatura a convertir.", Toast.LENGTH_LONG).show();
+            limpiarCheckBoxes();
+        }
     }
 
     public void habilitarCheckBoxes(boolean c, boolean f, boolean k, boolean r){
