20459d189a85080cad5ad1ade13fa4aa6377bbf6
Cristofer32513 <cristofer32513@gmail.com>
2019-05-10 21:13:13 -0500

    Se agregaron las variables con las que se trabajara a lo largo de todo el proyecto.

--- app/src/main/java/itsj/programas/conversordetemperaturas_radiosychecks/MainActivity.java
+++ app/src/main/java/itsj/programas/conversordetemperaturas_radiosychecks/MainActivity.java
@@ -2,8 +2,20 @@ package itsj.programas.conversordetemperaturas_radiosychecks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText cajaEntrada, cajaC, cajaF, cajaK, cajaR;
+    RadioButton radioC, radioF, radioK, radioR;
+    CheckBox checkC, checkF, checkK, checkR;
+    DecimalFormat redondear =new DecimalFormat("###,###,##0.####");
+    String respaldo="";
+    double tempEntrada, tempSalida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
